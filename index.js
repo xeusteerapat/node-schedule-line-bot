cdc6dc642cb14fcdebcfbcb73914a42223b37095
xeusteerapat <xeus085@gmail.com>
2020-12-14 21:34:25 +0700

    [add] user route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { broadcastText } = require('./controllers/text');
+const { createUser } = require('./controllers/user');
 const { cronJob } = require('./schedule/cronJob');
 
 const connectDB = require('./config/db');
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/text', broadcastText);
+app.post('/user', createUser);
 
 const { PORT } = process.env;
 
